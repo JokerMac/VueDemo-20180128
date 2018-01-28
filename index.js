2453a663060cb1ecb942a81308e6fc2ad73c115a
Joker <1185202783@qq.com>
2018-01-28 22:16:11 +0800

    v-on，v-bind，v-html的使用。

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        test: 'hello vue!'
+        test: 'hello vue!',
+        website: 'https://cn.bing.com/',
+        websiteTag: '<a href="www.baidu.com">百度一下</a>',
+        mouseX: 0,
+        mouseY: 0
     },
     methods: {
         helloWorld: function () {
-            console.error('hello wolrd');
+            // console.error('hello wolrd');
         },
-        hey:function(){
-            console.warn('hey');
+        hey: function (a) {
+            console.warn('hey ' + a);
+        },
+        updateXY: function (event) {
+            this.mouseX = event.offsetX;
+            this.mouseY = event.offsetY;
         }
     }
 });
